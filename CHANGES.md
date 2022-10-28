66ba3f3da764507ca9c7d0625dc5fc8c90e0521e
Mark Farnsworth <farnsworth2008@gmail.com>
2022-10-28 18:32:01 +0000

    0.1.9-s3d-1011 Changed
    - Added `s3d-flow-json` in `changes` module

--- CHANGES.md
+++ CHANGES.md
@@ -20,5 +20,5 @@ Recently completed and planned work is tracked here.
 ## [0.1.8](.)
 - Updated `aws` provider
 
-## [0.1.9-s3d-1010](.)
+## [0.1.9-s3d-1011](.)
 - Added `s3d-flow-json` in `changes` module
--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "name" {
-  source = "github.com/s3d-club/terraform-external-name?ref=0.1.10-s3d-1014"
+  source = "github.com/s3d-club/terraform-external-name?ref=0.1.10-s3d-1015"
 
   context = var.name_prefix
   path    = path.module
